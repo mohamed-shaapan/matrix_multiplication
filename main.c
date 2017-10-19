75aa52ce4162a7085c650164945cb7c7a8dabfd8
mohamed shaapan <mohamed.shaapan.1@gmail.com>
2017-10-19 13:49:02 +0000

    Beta 1.1 compare read files names

--- main.c
+++ main.c
@@ -7,81 +7,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-
-
-// internal functions
-// *******************************************
-void print_matrix(int *dims, int **matrix){
-    
-    int row_index;
-    int col_index;
-
-    for(row_index=0; row_index<dims[0]; row_index++){
-
-        for(col_index=0; col_index<dims[1]; col_index++){
-            printf("%d\t", matrix[row_index][col_index]);
-        }
-
-        printf("\n");
-    }
-}
+#include <string.h>
 
 
 // main function
 // *******************************************
 int main(int argc, char *argv[]){
 
+    // declare variables *************************************
+    char matrix_a_dir[100]="io_matrices/";
+    char matrix_b_dir[100]="io_matrices/";
+    char matrix_c_dir[100]="io_matrices/";
+    int **matrix_a; int **matrix_b; int **element_wise_result; int **row_wise_result;
+    int *matrix_a_dims; int *matrix_b_dims; int *element_wise_dims; int *row_wise_dims;
+    struct timeval element_start, element_stop;
+    struct timeval row_start, row_stop;
+    int element_num_threads; int row_num_threads;
+
     // 01 - get matrix file names
-    char *matrix_a_dir; char *matrix_b_dir; char *matrix_c_dir;
     if( argc > 1 ){
     	// use given file names
-    	matrix_a_dir=argv[0];
-    	matrix_b_dir=argv[1];
-    	matrix_c_dir=argv[2];
+        strcat(matrix_a_dir, argv[1]);
+        strcat(matrix_b_dir, argv[2]);
+        strcat(matrix_c_dir, argv[3]);
     }
     else{
-      // use default file names
-    	matrix_a_dir="io_matrices/a.txt";
-      	matrix_b_dir="io_matrices/b.txt";
-      	matrix_c_dir="io_matrices/c.txt";
+        // use default file names
+    	strcat(matrix_a_dir, "a.txt");
+        strcat(matrix_b_dir, "b.txt");
+        strcat(matrix_c_dir, "c.txt");
     }
  
     // 02 - load matrix into memory **************************
-    int **matrix_a; int *matrix_a_dims;
     load_matrix(matrix_a_dir, &matrix_a_dims, &matrix_a);
-    //print_matrix(matrix_a_dims, matrix_a);
-
-    int **matrix_b; int *matrix_b_dims;
     load_matrix(matrix_b_dir, &matrix_b_dims, &matrix_b);
-    //print_matrix(matrix_b_dims, matrix_b);
 
-
-    // apply multiplication algorithm ************************
+    // 03 - apply multiplication algorithm *******************
     //element-wise threading
-    struct timeval element_start, element_stop;
-    int **element_wise_result;
-    int *element_wise_dims;
-
     gettimeofday(&element_start, NULL); // start checking time
     multiply_matrices_element(matrix_a, matrix_a_dims, matrix_b, matrix_b_dims, &element_wise_result, &element_wise_dims);
     gettimeofday(&element_stop, NULL); // stop checking time
-    
+    element_num_threads=element_wise_dims[0]*element_wise_dims[1];
     // row-wise threading
-    struct timeval row_start, row_stop;
-    int **row_wise_result;
-    int *row_wise_dims;
-
     gettimeofday(&row_start, NULL); // start checking time
     multiply_matrices_row(matrix_a, matrix_a_dims, matrix_b, matrix_b_dims, &row_wise_result, &row_wise_dims);
     gettimeofday(&row_stop, NULL); // start checking time
+    row_num_threads=row_wise_dims[0];
 
     // 04 - store result ************************************
     store_matrix(element_wise_dims, element_wise_result, matrix_c_dir);
 
-
     // 05 - display compare result **************************
-    printf("\nElement Multp Time (micro): %lu\n", element_stop.tv_usec - element_start.tv_usec);
-    printf("\nRow Multp Time (micro): %lu\n", row_stop.tv_usec - row_start.tv_usec);
+    printf("\nElement-Wise Method *******************\n");
+    printf("Threads Created:\t%d\n", element_num_threads);
+    printf("Time Taken (Micro): \t%lu", element_stop.tv_usec - element_start.tv_usec);
+
+    printf("\n\nRow-Wise Method ***********************\n");
+    printf("Threads Created :\t%d\n", row_num_threads);
+    printf("Time Taken (Micro): %lu\n", row_stop.tv_usec - row_start.tv_usec);
 
     
     return 0;
--- try.c
+++ try.c
@@ -1,30 +1,43 @@
 // import libraries
 // ********************************************
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/time.h>
-
-int main(){
-
-	struct timeval stop, start;
 
 
-	gettimeofday(&start, NULL); //start checking time
-	
-	printf("\nPlease Enter Your Age : \n");
+// main function
+// *******************************************
+int main(int argc, char *argv[]){
 
-	gettimeofday(&stop, NULL); //end checking time
+    // declare variables *************************************
+    char matrix_a_dir[100]="io_matrices/";
+    char matrix_b_dir[100]="io_matrices/";
+    char matrix_c_dir[100]="io_matrices/";
+
+    // 01 - get matrix file names
+    if( argc > 1 ){
+    	// use given file names
+        strcat(matrix_a_dir, argv[1]);
+        strcat(matrix_b_dir, argv[2]);
+        strcat(matrix_c_dir, argv[3]);
+    }
+    else{
+        // use default file names
+    	strcat(matrix_a_dir, "a.txt");
+        strcat(matrix_b_dir, "b.txt");
+        strcat(matrix_c_dir, "c.txt");
+    }
+
+    printf("\nFile A : %s", matrix_a_dir);
+    printf("\nFile B : %s", matrix_b_dir);
+    printf("\nFile C : %s", matrix_c_dir);
+    printf("\n");
 
 
+    
+    return 0;
+  }
 
-
-
-	printf("Seconds taken %lu\n", stop.tv_sec - start.tv_sec);
-	printf("Microseconds taken: %lu\n", stop.tv_usec - start.tv_usec);
-
-	return 0;
-
-}
 
 
 
